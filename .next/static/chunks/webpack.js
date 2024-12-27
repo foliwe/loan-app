7781418f806245c98a487808ad8f425151ae5437
foliwe <foliwe@gmail.com>
2024-12-27 17:31:11 +0100

    new commit all

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "65461460315c182c"; }
+/******/ 		__webpack_require__.h = function() { return "79ef4789cdd89761"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
