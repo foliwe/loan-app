6a3f06408f80955303d3e5a6cc4f79f8d18c9d2a
foliwe <foliwe@gmail.com>
2024-12-27 17:12:52 +0100

    all new commit

--- .next/types/app/apply/page.ts
+++ .next/types/app/apply/page.ts
@@ -1,8 +1,8 @@
-// File: C:\Users\foliw\CascadeProjects\loan-web-app\src\app\about\page.tsx
-import * as entry from '../../../../src/app/about/page.js'
+// File: C:\Users\foliw\CascadeProjects\loan-web-app\src\app\apply\page.tsx
+import * as entry from '../../../../src/app/apply/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../src/app/about/page.js')
+type TEntry = typeof import('../../../../src/app/apply/page.js')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
